529fcad8f18253ad83dceb07b4f5ab67fb5d8eb8
michel <michel@maas-fam.nl>
2023-04-16 08:55:54 +0200

    Wijzigingen in startscripts

--- bankbook-frontend/src/main/resources/bin/startHeadless.sh
+++ bankbook-frontend/src/main/resources/bin/startHeadless.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-dir="${PWD%%bank-book*}bank-book${project.version}"
+dir="${PWD%%bank-book*}bank-book-${project.version}"
 java -Dserver.port=80 -Dmode=headless -Dfile.encoding=UTF-8 -Xms512m -Xmx768m -jar -Dapp.dir="$dir" $dir/lib/bankbook-frontend-${project.version}-exec.war > $dir/bankbook.log &
